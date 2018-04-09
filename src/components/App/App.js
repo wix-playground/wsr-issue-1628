7c185eaa8e185b385514063ea77a48261e272b4d
argshook <argshook@gmail.com>
2018-04-09 13:59:58 +0300

    init

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Text from 'wix-style-react/Text';
+import Button from 'wix-style-react/Button';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Text>Hello from Text</Text>
+        <Button>Hello from Button</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
